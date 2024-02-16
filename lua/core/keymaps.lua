e3f37b2576e376cccfd815141bede6416aa72792
syndagate <daniel4w@mail.de>
2024-02-17 00:18:21 +0100

    update keybindings

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,11 +3,32 @@ local wk = require("which-key")
 vim.keymap.set('n', '<leader>h', '<cmd>noh<cr>', { desc = 'disable word highlight'})
 vim.keymap.set('n', '<leader>t', '<cmd>ToggleTerm<cr>', { desc = 'toogle terminal'})
 vim.keymap.set('n', '<leader>q', '<cmd>q<cr>', { desc = 'close', silent = false })
-vim.keymap.set('n', '<leader>s', '<cmd>w<cr>', { desc = 'save', silent = false })
+vim.keymap.set('n', '<leader>w', '<cmd>w<cr>', { desc = 'save', silent = false })
 vim.keymap.set('n', '<leader>z', '<cmd>wq<cr>', { desc = 'save and close', silent = false })
 
+-- Split window management
+vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
+vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close split window
+vim.keymap.set("n", "<C-Down>", "<C-w>-") -- make split window height shorter
+vim.keymap.set("n", "<C-Up>", "<C-w>+") -- make split windows height taller
+vim.keymap.set("n", "<C-Left>", "<C-w>>5") -- make split windows width bigger
+vim.keymap.set("n", "<C-Right>", "<C-w><5") -- make split wicdows width smaller
+vim.keymap.set("n", "<M-Right>", "<C-w>l") -- switch to right split window
+vim.keymap.set("n", "<M-l>", "<C-w>l") -- switch to right split window
+vim.keymap.set("n", "<M-Left>", "<C-w>h") -- switch to left split window
+vim.keymap.set("n", "<M-h>", "<C-w>h") -- switch to left split window
+
+
+vim.keymap.set("n", "<M-Up>", "<C-w>k") -- switch to upper split window
+vim.keymap.set("n", "<M-k>", "<C-w>k") -- switch to upper split window
+vim.keymap.set("n", "<M-Down>", "<C-w>j") -- switch to lower split window
+vim.keymap.set("n", "<M-j>", "<C-w>j") -- switch to lower split window
 
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
+vim.keymap.set("n", "<leader>er", ":NvimTreeFocus<CR>") -- toggle focus to file explorer
+vim.keymap.set("n", "<leader>ef", ":NvimTreeFindFile<CR>") -- find file in file explorer
 
 -- telescope --
 local bi = require("telescope.builtin")
